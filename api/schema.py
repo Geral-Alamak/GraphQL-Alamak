274e2b3bc58f76ed7653f840f44b42b2b3edd80c
5803024020 <geraldo-l.inf24@ukwms.ac.id>
2026-09-14 17:44:15 +0700

    Update, ganti print value

--- api/schema.py
+++ api/schema.py
@@ -58,10 +58,9 @@ def resolve_author(*_, id):
 
 @query.field("books")
 def resolve_books(*_):
-    # This will now explicitly show up when you check the "Warning" filter box
     logger.warning("Executing resolve_books...") 
     result = _execute("SELECT * FROM books")
-    logger.warning(f"Data retrieved: {len(result)} records.")
+    logger.warning(f"Data retrieved: {len(result)} kali dia ngulang/looping.")
     return result
 
 @query.field("book")
